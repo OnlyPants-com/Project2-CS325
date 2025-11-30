e8619f8d85c685f7748b2274e76b263d92eb4d0d
OnlyPants-com <hcummins04@gmail.com>
2025-11-29 18:46:31 -0600

    Added a concrete version of embedding using OpenAI

---
+++ embedding_service.py
@@ -0,0 +1,16 @@
+"""Concrete implementation of IEmbeddingService using OpenAI."""
+from typing import List
+from openai import OpenAI #type: ignore
+from interfaces import IEmbeddingService
+
+class OpenAIEmbeddingService(IEmbeddingService):
+    def __init__(self, api_key: str, model: str = "text-embedding-3-small"):
+        self.client = OpenAI(api_key=api_key)
+        self.model = model
+    
+    def get_embedding(self, text: str) -> List[float]:
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=text
+        )
+        return response.data[0].embedding
