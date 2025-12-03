347dd5c00c2f1dd29a511e7e247f519875018d6c
OnlyPants-com <hcummins04@gmail.com>
2025-12-03 17:41:15 -0600

    Updated to use .env file

--- embedding_service.py
+++ embedding_service.py
@@ -2,10 +2,14 @@
 from typing import List
 from openai import OpenAI #type: ignore
 from interfaces import IEmbeddingService
+import os
+from dotenv import load_dotenv #type: ignore
+load_dotenv()
+
 
 class OpenAIEmbeddingService(IEmbeddingService):
     def __init__(self, api_key: str, model: str = "text-embedding-3-small"):
-        self.client = OpenAI(api_key=api_key)
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         self.model = model
     
     def get_embedding(self, text: str) -> List[float]:
